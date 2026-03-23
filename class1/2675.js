e9b6aa5c7b2a44a5b8f4cbe8a8d7b91216a325cb
hanyiseul <seulli_hys@daum.net>
2026-03-24 08:26:22 +0900

    class1 - 숫자열 반복

---
+++ class1/2675.js
@@ -0,0 +1,16 @@
+let input = require('fs').readFileSync('2675.txt').toString().trim().split('\n'); 
+
+let line = Number(input[0]);
+let result = "";
+
+for(let i = 1; i <= line; i++) {
+  let repeat = Number(input[i].split(" ")[0]);
+  let char = input[i].split(" ")[1];
+  for(let j = 0; j < char.length; j++) {
+    for(let k = 0; k < repeat; k++) {
+      result += char[j];
+    }
+  }
+  result += '\n';
+}
+console.log(result.trim()); // 마지막에 개행이 붙지 않게 하기 위해
