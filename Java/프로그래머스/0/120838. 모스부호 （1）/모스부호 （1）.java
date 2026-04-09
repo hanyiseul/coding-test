de963e1c3906bebdd14d865183c85923168f87fb
hanyiseul <54259614+hanyiseul@users.noreply.github.com>
2026-04-09 14:01:48 +0900

    [level 0] Title: 모스부호 (1), Time: 12.65 ms, Memory: 86.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String letter) {
+        String answer = "";
+        String[] arr = letter.split(" ");
+        String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---",
+"-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-",
+"..-","...-",".--","-..-","-.--","--.."};
+        
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < morse.length; j++) { 
+                if(arr[i].equals(morse[j])) answer += (char)('a'+j);
+            }
+        }
+        return answer;
+    }
+}
