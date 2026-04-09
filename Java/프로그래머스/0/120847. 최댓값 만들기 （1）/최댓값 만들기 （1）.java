4d1a7a84a19b2de4894bd686301233c858d97276
hanyiseul <54259614+hanyiseul@users.noreply.github.com>
2026-04-09 16:05:18 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.40 ms, Memory: 79 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int result = 0;
+        
+        for(int i = 0; i < numbers.length; i++){
+            for(int j = 0; j < numbers.length; j++) {
+                if(numbers[i]*numbers[j] > result && i!=j) {
+                    result = numbers[i]*numbers[j];
+                }
+                if(numbers.length == 2) {
+                    result = numbers[i]*numbers[j];
+                }
+            }
+        }
+        
+        return result;
+    }
+}
