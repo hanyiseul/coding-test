8714ba111f4fd2c8bfe7e16ef0f9befb2614ae3e
hanyiseul <54259614+hanyiseul@users.noreply.github.com>
2026-04-13 10:59:06 +0900

    [level 0] Title: 주사위의 개수, Time: 0.02 ms, Memory: 82.2 MB -BaekjoonHub

--- Java/프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
+++ Java/프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -1,10 +1,6 @@
 class Solution {
     public int solution(int[] box, int n) {
-        int answer = 1;
-        
-        for(int i=0; i < box.length; i++) {
-            answer *= box[i]/n;
-        }
+        int answer = 0;
         return answer;
     }
 }
