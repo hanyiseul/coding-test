1c2e7f4cb3123802f84c39d6410ae29c85bd3701
hanyiseul <54259614+hanyiseul@users.noreply.github.com>
2026-04-29 11:40:55 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.24 ms, Memory: 85.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String[] numbers = my_string.split("[^0-9]+");
+        for(String num : numbers) {
+            if(!num.equals("")) {
+                answer += Integer.parseInt(num);
+            }
+        }
+        return answer;
+    }
+}
