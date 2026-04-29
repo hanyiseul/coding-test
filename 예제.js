605e137a79d06dd494835090e72c5c750492e8af
hanyiseul <seulli_hys@daum.net>
2026-04-29 13:49:51 +0900

    삭제

--- 예제.js
+++
@@ -1,5 +0,0 @@
-// 배열
-let input = require('fs').readFileSync('2438.txt').toString().trim().split('\n'); 
-
-// 문자열
-let input = require('fs').readFileSync('1152.txt').toString().trim(); 
--- 제출.js
+++
@@ -1,5 +0,0 @@
-// 배열
-let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n'); 
-
-// 문자열
-let input = require('fs').readFileSync('/dev/stdin').toString().trim(); 
