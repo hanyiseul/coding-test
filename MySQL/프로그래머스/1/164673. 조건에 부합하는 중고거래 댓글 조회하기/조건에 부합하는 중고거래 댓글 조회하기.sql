2cf87c7f0683597e60ad116367ff16f768363885
hanyiseul <54259614+hanyiseul@users.noreply.github.com>
2026-05-03 18:24:53 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ MySQL/프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,12 @@
+select 
+    B.TITLE, 
+    B.BOARD_ID, 
+    REPLY_ID, 
+    R.WRITER_ID, 
+    R.CONTENTS, 
+	DATE_FORMAT(R.CREATED_DATE, '%Y-%m-%d')
+from USED_GOODS_BOARD B 
+    join USED_GOODS_REPLY R 
+    on B.BOARD_ID = R.BOARD_ID
+where DATE_FORMAT(B.CREATED_DATE, '%Y-%m') = '2022-10'
+order by R.CREATED_DATE, TITLE
